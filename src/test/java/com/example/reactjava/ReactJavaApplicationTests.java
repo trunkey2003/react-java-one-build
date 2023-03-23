05c08978b35ffb6c81accd3002ff53c8664c7d46
Ho Quang Lam <21521049@gm.uit.edu.vn>
2023-03-23 16:35:29 +0700

    first commit

---
+++ src/test/java/com/example/reactjava/ReactJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
