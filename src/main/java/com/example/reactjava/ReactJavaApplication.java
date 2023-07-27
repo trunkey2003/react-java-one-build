290b91d7a12b891892d3a1e4c05af10f6513a52b
trunkey2003 <21521049@gm.uit.edu.vn>
2023-07-27 11:33:44 +0700

    [ready to run] update to vite react 18 + spring

---
+++ src/main/java/com/example/reactjava/ReactJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactJavaApplication.class, args);
+	}
+
+}
